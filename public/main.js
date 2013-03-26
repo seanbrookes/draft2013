aae97e7573e9dabdc5894565ce152dc0d70af12f
Sean Brookes <sbrookes@beachair.ca>
2013-03-26 07:35:21 -0700

    socket io fix 2

--- public/main.js
+++ public/main.js
@@ -33,7 +33,7 @@ require.config({
         'admin'         : 'modules/admin/admin-module',
         'index'         : 'modules/index/index-module',
         'countdown'     : 'scripts/lib/plugins/kkcountdown',
-        'socket.io'     : '/socket.io/socket.io',
+        //'socket.io'     : '/socket.io/socket.io',
         'chat'          : 'modules/chat/chat-module',
         'chatlib'       : 'scripts/lib/plugins/chat.io',
         'pageheader'    : 'modules/pageheader/pageheader-module',
